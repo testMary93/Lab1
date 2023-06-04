32248c01f238270bd87b878815019cefede989e1
bychkova <masha.by4kova@mail.ru>
2023-06-04 18:16:49 +0300

    func call

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -4,7 +4,10 @@ class Program
 {
     static void Main()
     {
-        
+        int number = 5;
+        int factorial = CalculateFactorial(number);
+
+        Console.WriteLine("Факториал числа {0} равен {1}", number, factorial);
     }
     static int CalculateFactorial(int n)
     {
