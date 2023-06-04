670767cf6e40710b0029171fef7deef29f4cf173
bychkova <masha.by4kova@mail.ru>
2023-06-04 18:15:50 +0300

    factorial method

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static int CalculateFactorial(int n)
     {
-        
+        if (n == 0)
+            return 1;
+        else
+            return n * CalculateFactorial(n - 1);
     }
 }
